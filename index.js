2a052801ff08883bf8fd80a31fca91b778cde388
kazrkb <kazrkb@gmail.com>
2025-07-22 01:44:11 +0600

    created the post api

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const users = [
+    {id:1, name:'Sabana', email:'sabana@yahoo.com'},
+    {id:2, name:'Zohir', email:'zohir@outlook.com'},
+    {id:3, name:'Kasem', email:'kasem@ygmail.com'},
+]
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Users Management Server is Running')
+})
+
+app.get('/users', (req,res)=>{
+    res.send(users);
+})
+
+app.post('/users')
+
+
+app.listen(port, ()=>{
+    console.log(`server is running on port : ${port}`)
+})
