ac6c0d2fd4ab94d2a5d0201d3ad21097f7534d97
kazrkb <kazrkb@gmail.com>
2025-07-22 13:46:02 +0600

    push and send the user

--- __steps__.js
+++ __steps__.js
@@ -1,3 +1,8 @@
 /**
  * 1. create a post api in the srever side
+ * 2. client side send data via post api
+ * 3. set fetch method inside the fetch options(second parameter of fetch)
+ * 4. option will have 3 things: method, headers,body
+ * 5. method: 'POST', headers: {'content-type':'application/json'}, body: JSON.stringify
+ * 6. on the server side dont forget to use express.json() middleware
  */
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-
+//middleware
 app.use(cors())
+app.use(express.json())
 
 const users = [
     {id:1, name:'Sabana', email:'sabana@yahoo.com'},
@@ -24,6 +25,10 @@ app.get('/users', (req,res)=>{
 app.post('/users', (req, res) => {
     console.log('Post API called');
     console.log(req.body);
+    const newUser = req.body
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    res.send(newUser)
 })
 
 app.listen(port, ()=>{
